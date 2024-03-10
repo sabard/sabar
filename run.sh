2bed213faead9f814c0ceaa2f0e81607fc5f4c19
Sabar Dasgupta <s@bardasgupta.com>
2024-03-09 23:06:13 -0500

    add docker to run script

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-set -a && . ./variables-dev.env && set +a
-gunicorn --bind 0.0.0.0:5000 sabar_me:app
+#!/bin/bash
+
+if [ $1 == "--docker" ] ; then
+    docker build -t sabar_me .
+    docker run -d -p 5000:5000 sabar_me
+else
+    set -a && . ./variables-dev.env && set +a
+    gunicorn --bind 0.0.0.0:5000 sabar_me:app
+
+fi
